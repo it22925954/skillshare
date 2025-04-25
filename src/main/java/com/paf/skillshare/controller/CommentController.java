e598ed58bab38985d45d0b8182236c56f6133d05
it22925954 <127867845+it22925954@users.noreply.github.com>
2025-04-26 01:57:41 +0530

    add like controller

--- src/main/java/com/paf/skillshare/controller/CommentController.java
+++ src/main/java/com/paf/skillshare/controller/CommentController.java
@@ -1,12 +1,20 @@
 package com.paf.skillshare.controller;
 
-import com.paf.skillshare.model.Comment;
-import com.paf.skillshare.service.CommentService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.paf.skillshare.model.Comment;
+import com.paf.skillshare.service.CommentService;
 
 @RestController
 @RequestMapping("/api/comments")
--- src/main/java/com/paf/skillshare/repository/CommentRepository.java
+++ src/main/java/com/paf/skillshare/repository/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.paf.skillshare.repository;
 
-import com.paf.skillshare.model.Comment;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import com.paf.skillshare.model.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostId(Long postId);
