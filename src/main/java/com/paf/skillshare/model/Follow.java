7ccb56e207e094cfb84e1791482757c9d46cfa38
it22925954 <127867845+it22925954@users.noreply.github.com>
2025-05-06 17:28:13 +0530

    follow.java file update

--- src/main/java/com/paf/skillshare/model/Follow.java
+++ src/main/java/com/paf/skillshare/model/Follow.java
@@ -1,9 +1,17 @@
 package com.paf.skillshare.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
