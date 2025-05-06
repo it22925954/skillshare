96b621695ec12b16089498695c9b52ed82def945
Bashi Imalsha <it22296764@my.sliit.lk>
2025-05-06 13:31:28 +0530

    update postcontroller & postdto

--- src/main/java/com/paf/skillshare/model/Post.java
+++ src/main/java/com/paf/skillshare/model/Post.java
@@ -1,19 +1,28 @@
 package com.paf.skillshare.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Post {
 
     @Id
