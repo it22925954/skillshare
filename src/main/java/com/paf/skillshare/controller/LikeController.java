b95998d4c437b3b7605b2761cef11f1ed7c9e964
it22925954 <127867845+it22925954@users.noreply.github.com>
2025-05-06 13:44:31 +0530

    make changes in follow controller file

--- src/main/java/com/paf/skillshare/controller/LikeController.java
+++ src/main/java/com/paf/skillshare/controller/LikeController.java
@@ -1,13 +1,20 @@
 package com.paf.skillshare.controller;
 
-import com.paf.skillshare.model.Like;
-import com.paf.skillshare.service.LikeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paf.skillshare.model.Like;
+import com.paf.skillshare.service.LikeService;
 
 @RestController
 @RequestMapping("/api/likes")
@@ -33,3 +40,4 @@ public class LikeController {
         return ResponseEntity.ok(likeService.getLikesForPost(postId));
     }
 }
+
