18be43c995c16af246ea3e096d4e7c8b4f6c6d26
Bashi Imalsha <it22296764@my.sliit.lk>
2025-05-07 21:24:55 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import HomeFeed from './pages/HomeFeed';
-import CreatePost from './pages/CreatePost';
-import ProfilePage from './pages/ProfilePage';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const u = localStorage.getItem('user');
-    if (u) setUser(JSON.parse(u));
-  }, []);
-
   return (
-    <Router>
-      <Routes>
-        {!user ? (
-          <Route path="/" element={<CreatePost user={user} />} />
-        ) : (
-          <>
-            <Route path="/" element={<HomeFeed user={user} />} />
-            <Route path="/create" element={<CreatePost user={user} />} />
-            
-          </>
-        )}
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
